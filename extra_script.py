c496e005ca15dd3a218641692570249e8002a8c5
InuDeveloper <daviddevlife@gmail.com>
2025-08-12 14:10:30 -0400

    DAMN (Removed deprecated and unused code from the project)

--- extra_script.py
+++
@@ -1,9 +0,0 @@
-Import("env")
-
-# Add the include directory for esp32-camera
-env.Append(CPPPATH=[
-    "components/esp32-camera"
-])
-
-# Make sure the source files from the component are compiled
-env.Append(SRC_FILTER=["+<components/esp32-camera>"])
